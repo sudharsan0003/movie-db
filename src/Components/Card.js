48d79601944e13b84d1581476595c90c567b4b32
sudharsan0003 <sanmarly04@gmail.com>
2023-12-25 11:38:24 +0530

    final

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -18,35 +18,38 @@ const Card = (movie) => {
         <div>
           <button onClick={() => setVisible(true)}>About</button>
           <div className='flex justify-center items-center '>
-            <Model
-              isOpen={visible}
-              className='bg-slate-300 text-black lg:w-[90%] lg:h-[90%] lg:mt-10 lg:ml-10 sm:h-[90%] sm:w-[60%]'
-            >
-              <div className='m-5 flex justify-start '>
-                <button
-                  onClick={() => setVisible(false)}
-                  className='bg-white text-black rounded-full p-2  lg:mt-3 flex items-center gap-2 sm:m-1 sm:p-1 '
-                >
-                  <span>Back to</span>
-                  <CiLogout className='h-8 w-8 sm:h-4 ' />
-                </button>
-              </div>
-
-              <div className='flex justify-center items-center'>
-                <img
-                  src={img_path + movie.info.poster_path}
-                  className='h-60 w-60 mt-5 mb-5 sm:-mt-3 '
-                  alt='img'
-                />
-              </div>
-              <div>
-                <div className='flex flex-row justify-center items-center gap-5'>
-                  <h4 className='font-semibold text-center lg:mt-1 sm:-mt-5 '>
-                    {movie.info.title}
-                  </h4>
+            <Model isOpen={visible} id='model'>
+              <div className='bg-slate-600 w-full h-full rounded-lg'>
+                <div className='m-5 flex justify-start '>
+                  <button
+                    onClick={() => setVisible(false)}
+                    className='bg-white text-black rounded-full p-2  lg:mt-3 flex items-center gap-2 sm:m-1 sm:p-1 '
+                  >
+                    <span>Back to</span>
+                    <CiLogout className='h-8 w-8 sm:h-4 ' />
+                  </button>
                 </div>
+                <div className='flex justify-center items-center'>
+                  <img
+                    src={img_path + movie.info.poster_path}
+                    className='h-60 w-60 mt-5 mb-5 sm:-mt-3 '
+                    alt='img'
+                  />
+                </div>
+                <div>
+                  <div className='flex flex-row justify-center items-center gap-5 '>
+                    <h4 className='font-semibold text-center lg:mt-1 sm:mt-2 '>
+                      {movie.info.title}
+                    </h4>
+                    <p className='rating '>
+                      {movie.info.vote_average.toFixed()}
+                    </p>
+                  </div>
+                </div>
+                <h4 className='ml-10 lg:mt-1 sm:mt-1 '>
+                  {movie.info.overview}
+                </h4>
               </div>
-              <h4 className='ml-10 lg:mt-1 sm:mt-1 '>{movie.info.overview}</h4>
             </Model>
           </div>
         </div>
